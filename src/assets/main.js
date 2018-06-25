dd8a2fb4d08b924253948bfd27649793826548e3
VINT64 <vint64@mail.com>
2018-06-25 16:58:20 +0200

    new tech

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,27 +4,35 @@ const ejs = require('ejs');
 var port = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs');
-//app.set('views', __dirname + '/views');
+app.set('views', __dirname);
 app.engine('html', ejs.renderFile);
 
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/../public'));
-app.use('/links_lists', express.static(__dirname + '/links_lists'));
+// make express look in this directory for evrything
+app.use(express.static(__dirname));
 
 // set the home page route
+/*
 app.get('/', function(req, res) {
 
     // ejs render automatically looks in the views folder
-    res.render('index.html');
+    res.render('index');
 });
 app.get('/links_list', function(req, res) {
 
     // ejs render automatically looks in the views folder
-    res.render('links_list.html');
+    res.render('links_list');
 });
 
+app.get('/about', function(req, res) {
+	res.render('about');
+});
+
+ app.get('/not_found', function(req, res) {
+	res.render('not_found');
+});
+*/
 app.get('*', function(req, res) {
-	res.render('404.html');
+	res.render('404/index.html');
 });
 
 app.listen(port, function() {
