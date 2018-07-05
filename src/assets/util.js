39b702f069dfc733d8b2a34f79b36314d983809a
VINT64 <dzianis.yudzenka@gmail.com>
2018-07-05 16:09:09 +0200

    slow fix

--- src/assets/util.js
+++ src/assets/util.js
@@ -13,3 +13,29 @@ function loadJSON(file, fun, err) {
     };
     xobj.send(null);  
 }
+
+function Normalise(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+function addText(block, text){
+	block.appendChild(document.createTextNode(text));
+}
+
+function myNewBlock(type, cl){
+	var ret = document.createElement(type);
+	if (cl !== undefined)
+		ret.className = cl;
+	return ret;
+}
+
+function myNewText(type, text){
+	var ret = document.createElement(type);
+	if (text !== undefined)
+		addText(ret, text);
+	return ret;
+}
+
+function getFromTemp(temp, sel){
+	return temp.content.querySelector(sel).cloneNode(true);
+}
