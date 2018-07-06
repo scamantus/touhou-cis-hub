a153864ed6ae10e8c7500ed2d163bada686c64d3
VINT64 <vint64@mail.com>
2018-07-06 18:44:34 +0200

    little fixes

--- src/assets/main.js
+++ src/assets/main.js
@@ -7,30 +7,9 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname);
 app.engine('html', ejs.renderFile);
 
-// make express look in this directory for evrything
 app.use(express.static(__dirname));
 
-// set the home page route
-/*
-app.get('/', function(req, res) {
 
-    // ejs render automatically looks in the views folder
-    res.render('index');
-});
-app.get('/links_list', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('links_list');
-});
-
-app.get('/about', function(req, res) {
-	res.render('about');
-});
-
- app.get('/not_found', function(req, res) {
-	res.render('not_found');
-});
-*/
 app.get('*', function(req, res) {
 	res.render('404/index.html');
 });
